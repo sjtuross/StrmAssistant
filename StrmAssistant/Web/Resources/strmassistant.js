9984d5c762ec145b4a3207f19e16c838dd33082f
sjtuross <sjtuross@msn.com>
2024-11-16 16:47:21 +0800

    optimize: auto refresh after copy library

--- StrmAssistant/Web/Resources/strmassistant.js
+++ StrmAssistant/Web/Resources/strmassistant.js
@@ -1,4 +1,4 @@
-﻿define(['connectionManager', 'loading'], function (connectionManager, loading) {
+﻿define(['connectionManager', 'globalize', 'loading', 'toast'], function (connectionManager, globalize, loading, toast) {
 
     return {
         copy: function (libraryId) {
@@ -15,6 +15,14 @@
                 contentType: "application/json"
             }).finally(() => {
                 loading.hide();
+                const locale = globalize.getCurrentLocale().toLowerCase();
+                const confirmMessage = (locale === 'zh-cn') ? '\u590d\u5236\u5a92\u4f53\u5e93\u6210\u529f' : 
+                    (['zh-hk', 'zh-tw'].includes(locale) ? '\u8907\u88fd\u5a92\u9ad4\u5eab\u6210\u529f' : 'Copy Library Success');
+                toast(confirmMessage);
+                const itemsContainer = document.querySelector('.view .itemsContainer');
+                if (itemsContainer) {
+                    itemsContainer.notifyRefreshNeeded(true);
+                }
             });
         }
     };
